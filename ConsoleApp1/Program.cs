b2141fe05c8720de8f36f38a9ebdc9325431e77d
ayeshmohammed <ayeshmohammed37@gmail.com>
2026-07-02 18:35:24 +0300

    OOP Day4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,18 @@
 ﻿
+string s ="c";
+
+Console.WriteLine(s.Length);
+
+public class Creature
+{
+    public int Age;
+    public void Move()
+    {
+        Console.WriteLine("Creature is moving");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Creature is eating");
+    }
+}
