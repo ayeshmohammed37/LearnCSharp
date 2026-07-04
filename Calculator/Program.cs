5ba981aad47a7b88a5c3854cd1c2c1e4cd3c6c8a
ayeshmohammed <ayeshmohammed37@gmail.com>
2026-07-04 20:52:02 +0300

    Calculator: get two numbers from the user

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,15 @@
-﻿Console.WriteLine("Welcome to the Calculator Program!");
+﻿
+
+int num1 = default;
+int num2 = default;
+
+Console.WriteLine("Welcome to the Calculator Program!");
+
+Console.Write("Please Enter your first Number: ");
+num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Please Enter your second Number: ");
+num2 = int.Parse(Console.ReadLine());
+
 Console.ReadKey();
+
