528e09295b5219e45b91d0a6bc95f36e2b48f690
ayeshmohammed <ayeshmohammed37@gmail.com>
2026-07-04 21:02:55 +0300

    Calculator: apply operation and print result

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -15,5 +15,21 @@ num2 = int.Parse(Console.ReadLine());
 Console.Write("Enter number of Operation:\n1) +\n2) -\n3) *\n4) /\noperation: ");
 operation = int.Parse(Console.ReadLine());
 
+switch (operation)
+{
+    case 1:
+        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        break;
+    case 2:
+        Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+        break;
+    case 3:
+        Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+        break;
+    case 4:
+        Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+        break;
+}
+
 Console.ReadKey();
 
