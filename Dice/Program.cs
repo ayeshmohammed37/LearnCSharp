85bf71436cf32b2f9a76f7ff698bdd578bcd5f33
ayeshmohammed <ayeshmohammed37@gmail.com>
2026-07-05 07:43:55 +0300

    generating a random number

--- Dice/Program.cs
+++ Dice/Program.cs
@@ -1,5 +1,11 @@
 ﻿
+int playerRandomNumber;
+Random random = new Random();
+
 for (int i = 0; i < 10; i++)
 {
     Console.WriteLine("Press any Key to roll the dice");
+    Console.ReadLine();
+
+    playerRandomNumber = random.Next(1, 7);
 }
