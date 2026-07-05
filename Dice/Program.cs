2384c6502795ba89b0fb955b6f4e8ca493c80e13
ayeshmohammed <ayeshmohammed37@gmail.com>
2026-07-05 08:12:59 +0300

    record score for player1&2

--- Dice/Program.cs
+++ Dice/Program.cs
@@ -1,6 +1,10 @@
 ﻿
 int player1RandomNumber;
 int player2RandomNumber;
+
+int player1Score = 0;
+int player2Score = 0;
+
 Random random = new Random();
 
 for (int i = 0; i < 10; i++)
@@ -13,8 +17,26 @@ for (int i = 0; i < 10; i++)
 
     Console.WriteLine("...");
     Thread.Sleep(1000);
-    
+
     player2RandomNumber =random.Next(1, 7);
-    Console.WriteLine($"Enemy Rolled a {player2RandomNumber}");
+    Console.WriteLine($"Enemy Rolled a {player2RandomNumber}\n");
+
+    if (player1RandomNumber > player2RandomNumber)
+    {
+        player1Score++;
+        Console.WriteLine("player 1 wins this rouend");
+    }
+    else if (player1Score < player2Score)
+    {
+        player2Score++;
+        Console.WriteLine("Player2 wins this round");
+    }
+    else
+    {
+     Console.WriteLine("Draw");   
+    }
+
+    Console.WriteLine($"Player1 score: {player1Score}, Player2 score: {player2Score}");
+    Console.WriteLine();
 
 }
