977a91c0be08000e42df32733e04394b95e52843
ayeshmohammed <ayeshmohammed37@gmail.com>
2026-07-05 07:58:20 +0300

    generating and displaying the enemy’s number

--- Dice/Program.cs
+++ Dice/Program.cs
@@ -1,11 +1,20 @@
 ﻿
-int playerRandomNumber;
+int player1RandomNumber;
+int player2RandomNumber;
 Random random = new Random();
 
 for (int i = 0; i < 10; i++)
 {
     Console.WriteLine("Press any Key to roll the dice");
-    Console.ReadLine();
+    Console.ReadKey();
 
-    playerRandomNumber = random.Next(1, 7);
+    player1RandomNumber = random.Next(1, 7);
+    Console.WriteLine($"You Rolled a {player1RandomNumber}");
+
+    Console.WriteLine("...");
+    Thread.Sleep(1000);
+    
+    player2RandomNumber =random.Next(1, 7);
+    Console.WriteLine($"Enemy Rolled a {player2RandomNumber}");
+
 }
